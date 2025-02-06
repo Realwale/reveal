383b43db2bf5de614daf2aff201959fc2198cb41
Realwale <greatcharis26@gmail.com>
2025-02-06 18:15:58 +0100

    refactor

--- src/test/java/com/reveal/backend/RevealApplicationTests.java
+++ src/test/java/com/reveal/backend/RevealApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReavealApplicationTests {
+class RevealApplicationTests {
 
 	@Test
 	void contextLoads() {
